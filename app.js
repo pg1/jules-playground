7353a0ee199de33e5b4764e9f2ee3f2b1b50852d
pg1 <peter@zenbook.io>
2025-05-22 07:19:42 +0200

    Some old code

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/test", (req, res) => res.json({ ok: true }));
+
+app.listen(3000, () => {
+  console.log("Test started.");
+});
